dbf229554786146c96c48935a7976731256bbbcb
gtaras102 <64856633+gtaras102@users.noreply.github.com>
2023-05-26 15:40:00 +0300

    Scenes set up finished

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    
+    public void SceneLoader()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+}
